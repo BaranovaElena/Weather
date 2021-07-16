e78ad6138a491038fc7c4b3d002e61e040f8d8ce
BaranovaElena <baranovaeaa@yandex.ru>
2021-07-16 16:05:36 +0200

    добавила observer в фрагмент, в ViewModel кинула ему тост

--- app/src/main/java/com/example/weather/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/weather/ui/main/MainViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.weather.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()) :
+    ViewModel() {
+
+    fun getData(): LiveData<Any> {
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = "SEND DATA"
+    }
 }
