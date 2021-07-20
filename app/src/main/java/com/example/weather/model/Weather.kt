50a058aef36bfe3bb0adf49d09ed25f9af1fbcc6
BaranovaElena <baranovaeaa@yandex.ru>
2021-07-20 23:13:49 +0200

    передача данных через bundle от адаптера в фрагмент с погодой, обработка клика по городу

--- app/src/main/java/com/example/weather/model/Weather.kt
+++ app/src/main/java/com/example/weather/model/Weather.kt
@@ -1,5 +1,9 @@
 package com.example.weather.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Weather(
     val city: City = City(
         "Москва",
@@ -8,4 +12,4 @@ data class Weather(
     ),
     val temperature: Int = 0,
     val feelsLike: Int = 0
-)
+): Parcelable
