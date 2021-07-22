b932fb727d02102866d75804f60264a5318fabf0
Елена Баранова <baranovaeaa@yandex.ru>
2021-07-23 00:27:18 +0300

    добавлена навигация. стартовым экраном сделала детальное отображение погоды в москве DetailsFragment берет погоду в москве по умолчанию не из viewModel, надо исправить

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.weather.R
 import com.example.weather.databinding.MainActivityBinding
 import com.example.weather.model.RepoImpl
